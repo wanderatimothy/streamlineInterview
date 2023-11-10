051fa1f04e4bc20738618964edf2fce2509db2cc
Wandera Timothy <46409284+wanderatimothy@users.noreply.github.com>
2023-11-10 15:26:14 +0300

    Refactor - simplifying the codebase

--- Anagram.php
+++ Anagram.php
@@ -1,43 +1,15 @@
 <?php 
 
 
-function anagram(string $str1, string $str2) : bool
+function anagram( $str1,$str2) : bool
 {
   
     $merge =  $str1 . $str2;
 
-    if(strlen($merge) === 0){
-        return true;
-    }
 
     $singleSentence =  str_replace(' ','',strtolower($merge)); 
 
-    $matchCounter = [];
-
-    foreach( explode('',$singleSentence) as $c){
-
-        $keys = array_keys($matchCounter);
-
-        if(in_array($c ,$keys)){
-
-            $matchCounter[$c] += 1;
-        }
-        else{
-            $matchCounter[$c] = 1;
-        }
-
-    }
-
-    $values = array_values($matchCounter);
-
-    $total = 0;
-
-    for($i=0;$i < 0; $i++ ){
-
-        $total += $values[$i];
-    }
-   
-    return ($total%2) === 0 ;
+    return (strlen($singleSentence)%2) === 0 ;
  
 }
 
@@ -51,7 +23,7 @@ $string1 = 'listen';
 
 $string2 = 'silent';
 
-echo "Testing that string 1 : $str1  and string 2 : $string2  are anagarams <br/> <br/> ";
+echo "Testing that string 1 : $string1  and string 2 : $string2  are anagrams <br/> <br/> ";
 
 
 if(anagram($string1 , $string2)) {
