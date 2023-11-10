303df23e00c7c8447b08ebfdeeeea9d6ceb8e5bc
Wandera Timothy <46409284+wanderatimothy@users.noreply.github.com>
2023-11-10 16:24:39 +0300

    update - Refactoring code

--- question1/Anagram.php
+++ question1/Anagram.php
@@ -9,11 +9,34 @@ function anagram( $str1,$str2) : bool
 
     $singleSentence =  str_replace(' ','',strtolower($merge)); 
 
-    return (strlen($singleSentence)%2) === 0 ;
+    $characters = array_unique(str_split($singleSentence));
+
+    $matchCounter = [];
+
+    foreach(str_split($singleSentence)  as $c){
+
+        if(in_array($c , $characters)){
+
+            $matchCounter[$c] += 1;
+        }else{
+
+            $matchCounter[$c] = 1;
+        }
+    }
+
+
+    foreach(array_keys($matchCounter) as $k){
+
+        if(($matchCounter["$k"] % 2) === 1){
+
+            return false;
+        } 
+    }
+
+
+    return  true ;
  
 }
-
-
 
 
 // test 
@@ -24,7 +47,6 @@ $string1 = 'listen';
 $string2 = 'silent';
 
 echo "Testing that string 1 : $string1  and string 2 : $string2  are anagrams <br/> <br/> ";
-
 
 if(anagram($string1 , $string2)) {
 
